bcd973f117f6e8cdd873b4cdefed2ba22f1e3c4e
Kashyap Sharma <98746601+kashyapndps00@users.noreply.github.com>
2022-05-01 20:27:56 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: '5249293741:AAEEmbUu5PtGW62oRpzeFbqSy_mIa9L1NmQ',
-    admins: [5215163224,1210366533],
+    token: '5305514543:AAHkStdLdtmFIMZjd6NL8_o9vgQCs5uBCH4',
+    admins: [5215163224,1210366533,1442176965],
     curr:'INR'
 }
 
